1738c147c07069b3ef2a6ae8409f6dcad816d0d1
Xingan Wang <wangxgwxg@gmail.com>
2017-03-11 00:27:48 -0800

    refactor structure

---
+++ lib/cubemap-cut.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python
+
+from __future__ import print_function
+import sys
+import os.path
+from PIL import Image
+import zipfile
+
+if len(sys.argv) < 2:
+    print("Usage: cubemap-cut.py <filename.jpg|png>")
+    sys.exit(-1)
+
+infile = sys.argv[1]
+filename, original_extension = os.path.splitext(infile)
+file_extension = ".png"
+
+name_map = [ \
+     ["", "", "posy", ""],
+     ["negz", "negx", "posz", "posx"],
+     ["", "", "negy", ""]]
+
+try:
+    im = Image.open(infile)
+    print(infile, im.format, "%dx%d" % im.size, im.mode)
+
+    width, height = im.size
+
+    cube_size = width / 4
+
+    filelist = []
+    for row in range(3):
+        for col in range(4):
+            if name_map[row][col] != "":
+                sx = cube_size * col
+                sy = cube_size * row
+                fn = name_map[row][col] + file_extension
+                filelist.append(fn)
+                print("%s --> %s" % (str((sx, sy, sx + cube_size, sy + cube_size)), fn))
+                im.crop((sx, sy, sx + cube_size, sy + cube_size)).save(fn) 
+
+    zfname = filename + '.zip'
+    print("Creating zipfile: " + zfname)
+    zf = zipfile.ZipFile(zfname, mode='w')
+    try:
+        for fn in filelist:
+            zf.write(fn)
+        print("done")
+    finally:
+        zf.close()
+
+except IOError:
+    pass
